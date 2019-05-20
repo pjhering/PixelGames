b137c1800d4e5b4a663558362a9730b40f3d3edf
Pete Hering <pjhering@gmail.com>
2019-05-19 20:12:34 -0400

    working

--- pixel-maps/source/pixel/maps/ObjectLayer.java
+++ pixel-maps/source/pixel/maps/ObjectLayer.java
@@ -1,6 +1,55 @@
 package pixel.maps;
 
-public class ObjectLayer
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ObjectLayer implements Iterable<MapObject>
 {
-	
+	private final Set<MapObject> active;
+    private final Set<MapObject> toBeAdded;
+    private final Set<MapObject> toBeRemoved;
+
+    public ObjectLayer ()
+    {
+        this.active = new HashSet<> ();
+        this.toBeAdded = new HashSet<> ();
+        this.toBeRemoved = new HashSet<> ();
+    }
+
+    @Override
+    public Iterator<MapObject> iterator ()
+    {
+        return active;
+    }
+
+    public void add (MapObject obj)
+    {
+        toBeAdded.add (obj);
+    }
+
+    public void remove (MapObject obj)
+    {
+        toBeRemoved.add (obj);
+    }
+
+    public void update (long elapsedMillis)
+    {
+        if (!toBeRemoved.empty ())
+        {
+            active.removeAll (toBeRemoved);
+            toBeRemoved.clear ();
+        }
+
+        if (!toBeAdded.empty ())
+        {
+            active.addAll (toBeAdded);
+            toBeAdded.clear ();
+        }
+
+        for (MapObject obj : active)
+        {
+            obj.update (elapsedMillis);
+        }
+    }
 }
