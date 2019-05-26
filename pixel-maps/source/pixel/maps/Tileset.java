2b923f93c5dd4202915cb45a974349e339c1c2f7
Pete Hering <pjhering@gmail.com>
2019-05-25 20:59:07 -0400

    working

--- pixel-maps/source/pixel/maps/Tileset.java
+++ pixel-maps/source/pixel/maps/Tileset.java
@@ -5,9 +5,34 @@ import java.awt.image.BufferedImage;
 public class Tileset
 {
 	private Tile[] tiles;
+	private int tileSize;
 
 	public Tileset (BufferedImage image, int tileSize)
 	{
+		this.tileSize = tileSize;
+
+		int rows = image.getHeight () / tileSize;
+		int cols = image.getWidth () / tileSize;
+		int count = rows * cols;
+		int halfWay = rows / 2;
+		tiles = new Tile[count];
+		int i = 0;
+
+		for (int r = 0; r < rows; r++)
+		{
+			boolean blocked = r >= halfWay;
+			int y1 = r * tileSize;
+			int y2 = y1 + tileSize;
+
+			for (int c = 0; c < cols; c++)
+			{
+				int x1 = c * tileSize;
+				int x2 = x1 + tileSize;
+
+				tiles[i] = new Tile (image, x1, y1, x2, y2, blocked);
+				i++;
+			}
+		}
 	}
 
 	public Tile getTile(int i)
