8a11be5de936bd97ec992f450796326bd8d78934
Pete Hering <pjhering@gmail.com>
2019-05-26 16:29:47 -0400

    working

--- pixel-maps/source/pixel/maps/MapObject.java
+++ pixel-maps/source/pixel/maps/MapObject.java
@@ -7,11 +7,11 @@ public class MapObject
 {
 	public static enum Type { POINT, CIRCLE, RECTANGLE }
 
-	protected double x;
-	protected double y;
-	protected double width;
-	protected double height;
-	protected double radius;
+	protected double x = Double.NaN;
+	protected double y = Double.NaN;
+	protected double width = Double.NaN;
+	protected double height = Double.NaN;
+	protected double radius = Double.NaN;
 	protected Type type;
 	protected Map<String, Object> properties;
 
@@ -43,6 +43,108 @@ public class MapObject
 		this.y = y;
 		this.type = Type.POINT;
 		this.properties = new HashMap<> ();
+	}
+
+	public static boolean hits (MapObject a, MapObject b)
+	{
+		switch (a.getType ())
+		{
+			case POINT:
+				switch (b.getType ())
+				{
+					case POINT:
+						return pointHitsPoint (a, b);
+					case CIRCLE:
+						return pointHitsCircle (a, b);
+					case RECTANGLE:
+						return pointHitsRectangle (a, b);
+				}
+			case CIRCLE:
+				switch (b.getType ())
+				{
+					case POINT:
+						return pointHitsPoint (b, a);
+					case CIRCLE:
+						return circleHitsCircle (a, b);
+					case RECTANGLE:
+						return circleHitsRectangle (a, b);
+				}
+			case RECTANGLE:
+				switch (b.getType ())
+				{
+					case POINT:
+						return pointHitsRectangle (b, a);
+					case CIRCLE:
+						return circleHitsRectangle (b, a);
+					case RECTANGLE:
+						return rectangleHitsRectangle (a, b);
+				}
+		}
+
+		return false;
+	}
+
+	private static boolean pointHitsPoint (MapObject a, MapObject b)
+	{
+		return a.x == b.x && a.y == b.y;
+	}
+
+	private static double distSqr (MapObject a, MapObject b)
+	{
+		double x = a.x - b.x;
+		double y = a.y - b.y;
+		return x * x + y * y;
+	}
+
+	private static boolean pointHitsCircle (MapObject a, MapObject b)
+	{
+		double d = distSqr (a, b);
+		return d < b.radius * b.radius;
+	}
+
+	private static boolean pointHitsRectangle (MapObject a, MapObject b)
+	{
+		return a.x > b.x
+			&& a.x < b.x + b.width
+			&& a.y > b.y
+			&& a.y < b.y + b.height;
+	}
+
+	private static boolean circleHitsCircle (MapObject a, MapObject b)
+	{
+		double d = distSqr (a, b);
+		double r = a.radius + b.radius;
+		return d < r * r;
+	}
+
+	private static boolean circleHitsRectangle (MapObject c, MapObject r)
+	{
+		double testX = c.x;
+		double testY = c.y;
+
+		if (c.x < r.x)                testX = r.x;
+		else if (c.x > r.x + r.width) testX = r.x + r.width;
+
+		if (c.y < r.y)                testY = r.y;
+		else if (c.y > r.y + r.height) testY = r.y + r.height;
+
+		double distX = c.x - testX;
+		double distY = c.y - testY;
+		double distance = Math.sqrt((distX * distX) + (distY * distY));
+
+		if (distance <= c.radius)
+		{
+	       return true;
+		}
+	    return false;
+	}
+
+	private static boolean rectangleHitsRectangle (MapObject a, MapObject b)
+	{
+		return a.x < b.x + b.width
+			&& a.x + a.width > b.x
+			&& a.y < b.y + b.height
+			&& a.y + a.height > b.y;
 	}
 
 	public double getX ()
