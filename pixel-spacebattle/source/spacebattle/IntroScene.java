1df99da910b9cc8f75bdef5697cfa3238c05a8ef
Pete Hering <pjhering@gmail.com>
2019-06-15 09:51:53 -0400

    added new game

---
+++ pixel-spacebattle/source/spacebattle/IntroScene.java
@@ -0,0 +1,65 @@
+package spacebattle;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.util.Objects;
+import pixel.Assets;
+import pixel.Scene;
+import pixel.SceneManager;
+
+public class IntroScene extends Scene
+{
+    private final Assets a;
+    private final int width;
+    private final int height;
+    private boolean exit;
+    private final String TEXT;
+    private final int x;
+
+    public IntroScene (Assets a, int width, int height)
+    {
+        this.a = Objects.requireNonNull (a);
+        this.width = Math.abs (width);
+        this.height = Math.abs (height);
+        this.TEXT = a.getString (0);
+        this.x = (width - a.getFont (6).getWidth (TEXT)) / 2;
+    }
+
+    public void update (SceneManager mgr, long elapsedMillis)
+    {
+        if (exit)
+        {
+            mgr.replace (new GameScene (a, width, height));
+        }
+    }
+
+    public void render (Graphics g)
+    {
+        g.setColor (a.getColor (4));
+        g.fillRect (0, 0, width, height);
+        a.getFont (6).draw (g, TEXT, x, 200);
+    }
+
+    public void activate ()
+    {
+        exit = false;
+    }
+
+    public void deactivate ()
+    {
+    }
+
+    public void dispose ()
+    {
+    }
+
+    @Override
+    public void keyPressed (KeyEvent e)
+    {
+        if (e.getKeyCode () == KeyEvent.VK_ENTER)
+        {
+            exit = true;
+        }
+    }
+}
