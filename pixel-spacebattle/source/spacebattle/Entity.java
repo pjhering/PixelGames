b9be1c77cff383dd67c6dfdd41f3b8081f84ee19
Pete Hering <pjhering@gmail.com>
2019-06-17 06:28:03 -0400

    working

--- pixel-spacebattle/source/spacebattle/Entity.java
+++ pixel-spacebattle/source/spacebattle/Entity.java
@@ -7,13 +7,22 @@ public abstract class Entity
     protected double x;
     protected double y;
     protected double radius;
+    protected double deltaX, minDeltaX, maxDeltaX;
+    protected double deltaY, minDeltaY, maxDeltaY;
 
 	public Entity (double x, double y, double radius)
 	{
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
+        this.deltaX = this.deltaY = 0.0;
+        this.minDeltaX = this.minDeltaY = Double.MIN_VALUE;
+        this.maxDeltaX = this.maxDeltaY = Double.MAX_VALUE;
 	}
+
+	public abstract void update (long elapsedMillis);
+
+	public abstract void draw (Graphics g);
 
 	public boolean hits (Entity that)
 	{
@@ -25,7 +34,57 @@ public abstract class Entity
 		return dsq < rsq;
 	}
 
-	public abstract void update (long elapsedMillis);
+    public void accelerate (double xAmount, double yAmount)
+    {
+        deltaX = Math.min (maxDeltaX, Math.max (minDeltaX, deltaX + xAmount));
+        deltaY = Math.min (maxDeltaY, Math.max (minDeltaY, deltaY + yAmount));
+    }
 
-	public abstract void draw (Graphics g);
+    public void setDelta (double xAmount, double yAmount)
+    {
+        deltaX = Math.min (maxDeltaX, Math.max (minDeltaX, xAmount));
+        deltaY = Math.min (maxDeltaY, Math.max (minDeltaY, yAmount));
+    }
+
+    public void setMinDelta (double xAmount, double yAmount)
+    {
+        minDeltaX = xAmount;
+        minDeltaY = yAmount;
+    }
+
+    public void setMaxDelta (double xAmount, double yAmount)
+    {
+        maxDeltaX = xAmount;
+        maxDeltaY = yAmount;
+    }
+
+    public double getX ()
+    {
+        return this.x;
+    }
+
+    public void setX (double value)
+    {
+        this.x = value;
+    }
+
+    public double getY ()
+    {
+        return this.y;
+    }
+
+    public void setY (double value)
+    {
+        this.y = value;
+    }
+
+    public double getRadius ()
+    {
+        return this.radius;
+    }
+
+    public void setRadius (double value)
+    {
+        this.radius = value;
+    }
 }
