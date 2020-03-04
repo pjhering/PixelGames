d90738c80f1153ed6489a4ced30b4d41c31fafeb
Pete Hering <pjhering@gmail.com>
2020-03-03 21:05:17 -0500

    working on TODOs

--- pixel-spaceinvaders/source/spaceinvaders/GameScene.java
+++ pixel-spaceinvaders/source/spaceinvaders/GameScene.java
@@ -1,12 +1,13 @@
 package spaceinvaders;
 
 import java.awt.*;
+import java.awt.event.*;
 import pixel.*;
 
 public class GameScene extends Scene
 {
     private static final double PI = 3.1415926535897932;
-    
+
     private Dimension d;
     private Font largeFont = new Font ("Helvetica", Font.BOLD, 24);
     private Font smallFont = new Font ("Helvetica", Font.BOLD, 14);
@@ -50,35 +51,195 @@ public class GameScene extends Scene
     private int score;
     private int lives = 4;
     private boolean dying = false;
-    private int rotx, roty;
-    private int[] animpos, animdpos;
+    private int[] rotx, roty;
+    private int animpos, animdpos;
     private int speckcount;
     private int[] polyx, polyy;
     private int groundy = 210;
 
     public GameScene ()
     {
+        init ();
     }
 
-    public void update (SceneManager mgr, long elapsedMillis)
+    private void init ()
+    {
+        int i;
+
+        d = new Dimension (Main.WIDTH, Main.HEIGHT);
+
+        drawbomb = new boolean[maxbombs];
+        bombx = new int[maxbombs];
+        bomby = new int[maxbombs];
+        drawenemy = new boolean[rowsofenemies * enemiesperrow];
+        rotx = new int[rotsteps];
+        roty = new int[rotsteps];
+        polyx = new int[4];
+        polyy = new int[4];
+        groundy = d.height - 30;
+
+        enemyradius = (d.width - enemyxoffset) / (enemiesperrow - enemyxdelta);
+        for (i = 0; i < rotsteps; i++)
+        {
+            rotx[i] = (int)(enemyradius / 2 * Math.sin (2 * PI * i / rotsteps));
+            roty[i] = (int)(enemyradius / 2 * Math.cos (2 * PI * i / rotsteps));
+        }
+        GameInit ();
+        ingame = false;
+    }
+
+    private void FontMetricsInit (Graphics g)
+    {
+        g.setFont (smallFont);
+        fmsmall = g.getFontMetrics ();
+        g.setFont (largeFont);
+        fmlarge = g.getFontMetrics ();
+    }
+
+    private void GameInit ()
+    {
+        score = 0;
+        LevelInit ();
+        ingame = true;
+        curmaxbombs = 4;
+        lives = 3;
+    }
+
+    private void LevelInit ()
+    {
+        int i;
+        enemyx = enemyradius;
+        enemyy = enemyradius;
+        enemydx = 0;
+        animpos = 0;
+        animdpos = -1;
+        enemycount = 0;
+        enemydelay = 6;
+        speckcount = enemiesperrow * rowsofenemies;
+        for (i = 0; i < enemiesperrow * rowsofenemies; i++)
+        {
+             drawenemy[i] = true;
+        }
+        enemyspeed = 1;
+        dying = false;
+        LevelContinue ();
+    }
+
+    private void LevelContinue ()
+    {
+        int i;
+
+        bulletx = -1;
+        bullety = -1;
+        for (i = 0; i < maxbombs; i++)
+        {
+            drawbomb[i] = false;
+        }
+        bombcount = 0;
+        turretx = d.width / 2;
+    }
+
+    @Override
+    public void keyPressed (KeyEvent e)
+    {
+        int key = e.getKeyCode ();
+        char keyChar = e.getKeyChar ();
+
+        if (ingame)
+        {
+            switch (key)
+            {
+                case KeyEvent.VK_LEFT:
+                    turretx = -2;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    turretx = 2;
+                    break;
+                case KeyEvent.VK_ESCAPE:
+                    ingame = false;
+                    break;
+            }
+        }
+        else
+        {
+            if (keyChar == 's' || keyChar == 'S')
+            {
+                GameInit ();
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased (KeyEvent e)
+    {
+        int key = e.getKeyCode ();
+        char keyChar = e.getKeyChar ();
+
+        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_RIGHT)
+        {
+            turretdx = 0;
+        }
+
+        if (keyChar == ' ' && bullety < 0)
+        {
+            bullety = groundy - 7;
+            bulletx = turretx;
+        }
+    }
+
+    private void DrawSpecks () // TODO
     {
     }
 
-    public void render (Graphics g)
-    {
-        g.setColor (Color.GREEN);
-        g.fillRect (0, 0, Main.WIDTH, Main.HEIGHT);
-    }
-
-    public void activate ()
+    private void MoveSpecks () // TODO
     {
     }
 
-    public void deactivate ()
+    private void DoTurret () // TODO
     {
     }
 
-    public void dispose ()
+    private void DoBullet () // TODO
+    {
+    }
+
+    private void DoBombs () // TODO
+    {
+    }
+
+    private void CheckBombs () // TODO
+    {
+    }
+
+    private void ShowScore () // TODO
+    {
+    }
+
+    private void ShowIntroScene () // TODO
+    {
+    }
+
+    public void update (SceneManager mgr, long elapsedMillis) // TODO
+    {
+    }
+
+    public void render (Graphics g) // TODO
+    {
+        if (fmsmall == null || fmlarge == null)
+        {
+            FontMetricsInit (g);
+        }
+    }
+
+    public void activate () // NOT USED
+    {
+    }
+
+    public void deactivate () // NOT USED
+    {
+    }
+
+    public void dispose () // NOT USED
     {
     }
 }
