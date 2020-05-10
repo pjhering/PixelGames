f37bd93339d0ad7e031263fabbbe61820b2751db
Pete Hering <pjhering@gmail.com>
2020-05-10 16:39:22 -0400

    added point-line collision tests

--- pixel-app/source/pixel/geom/Utility.java
+++ pixel-app/source/pixel/geom/Utility.java
@@ -18,4 +18,26 @@ public class Utility
             && y1 > y2 - BUFFER
             && y1 < y2 + BUFFER;
     }
+
+    public static boolean collision (Point p, Line l)
+    {
+        double px = p.getX ();
+        double py = p.getY ();
+        double x1 = l.getA ().getX ();
+        double y1 = l.getA ().getY ();
+        double x2 = l.getB ().getX ();
+        double y2 = l.getB ().getY ();
+
+        return point_line_collision (px, py, x1, y1, x2, y2);
+    }
+
+    public static boolean point_line_collision (double px, double py, double x1, double y1, double x2, double y2)
+    {
+        float d1 = Point.distance (px, py, x1, y1);
+        float d2 = Point.distance (px, py, x2, y2);
+        float lineLen = Point.distance (x1, y1, x2, y2);
+
+        return d1 + d2 >= lineLen - BUFFER
+            && d1 + d2 <= lineLen + BUFFER;
+    }
 }
